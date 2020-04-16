9f3eab95273fd2f5c59355634e2f3818124b544e
criscoutinho02 <cris.coutinho02@gmail.com>
2020-04-15 22:23:33 -0300

    primeira rest api

---
+++ src/main/java/com/learningrest/demo/restservice/GreetingController.java
@@ -0,0 +1,22 @@
+package com.learningrest.demo.restservice;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s! Feliz %d anos!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name" , defaultValue="World") String name ,
+                             @RequestParam(value="age" ) int age){
+        return new Greeting(counter.incrementAndGet(), String.format(template,name,age));
+    }
+
+}
---
+++ src/test/java/com/learningrest/demo/RestwebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learningrest.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestwebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
